f57b3a529d9faa6129be43b53f0ab7401fdff81c
codelover404 <wajidbloggerno1@gmail.com>
2022-01-26 11:56:00 +0500

    first assigment

---
+++ assigment-one.py
@@ -0,0 +1,23 @@
+
+
+
+# swippint assigment
+
+num1 = 10
+num2 = 20
+
+# data swipped
+
+updatedNum1 = num2
+updatedNum2 = num1
+
+print("Before swipped the Value Of Num1 is :",num1)
+print("Before swipped the Value Of Num2 is :",num2)
+
+print("After swipped the Value Of Num1 is :",updatedNum1)
+print("After swipped the Value Of Num2 is :",updatedNum2)
+#
+#
+# print(updatedNum1)
+# print(updatedNum1)
+# print(updatedNum1)
